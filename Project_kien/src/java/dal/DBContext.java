9daa5509c2ad312f60f2d2526571150f04a438f1
kiên <91230993+kienphan1234@users.noreply.github.com>
2022-05-29 09:48:36 +0700

    tao cac packages
    tao cac packages

---
+++ Project_kien/src/java/dal/DBContext.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public abstract class DBContext<T> {
+    protected Connection connection;
+    public DBContext()
+    {
+        try {
+            String user = "kien123";
+            String pass = "12345";
+            String url = "jdbc:sqlserver://DESKTOP-47KD5D0\\SQLEXPRESS:1433;databaseName=Assment_prj301";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public abstract ArrayList<T> list();
+    public abstract T get(int id);
+    public abstract void insert(T model);
+    public abstract void update(T model);
+    public abstract void delete(T model);
+}
+
